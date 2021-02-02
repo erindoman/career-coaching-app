a69eff1f719ef3344969bef5c4cce3592fbcbbdb
erindoman <erin.doman@gmail.com>
2021-02-02 15:24:01 -0600

    add client model

--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,6 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def clients_index(request):
+    return render(request, 'clients/index.html', { 'clients': clients })
