cac6e5145b39a277cb816d0ba3e861c7ff735a2b
erindoman <erin.doman@gmail.com>
2021-02-03 13:48:53 -0600

    add auth

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('clients/', views.clients_index, name='index'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
