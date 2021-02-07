0d416fd8b3bfc11fb45213d95ca0d012eba83d7a
erindoman <erin.doman@gmail.com>
2021-02-06 19:41:42 -0600

    update button colors

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Client, Application, Skill
+from .models import Client, Application, Skill, Photo
 
 # Register your models here.
 admin.site.register(Client)
 admin.site.register(Application)
 admin.site.register(Skill)
+admin.site.register(Photo)
