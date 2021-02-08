44a09ebd7a1e289f6108d1caced6e6bd89b556d0
erindoman <erin.doman@gmail.com>
2021-02-08 15:33:39 -0600

    Config deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn careercoach.wsgi
--- careercoach/settings.py
+++ careercoach/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import environ
+environ.Env()
+environ.Env.read_env()
+
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2h=i&cen@hpy)1kayg7@jb6-ow#5-z4-3%g@2dj_5&w892i$wz'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +128,6 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = "/clients/"
 
 LOGOUT_REDIRECT_URL = '/'
+
+import django_on_heroku
+django_on_heroku.settings(locals())
