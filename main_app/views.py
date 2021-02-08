d063f5cc1943c6da6f3e5ec8e4b7e0646e97e282
erindoman <erin.doman@gmail.com>
2021-02-08 08:55:08 -0600

    clean up code

--- main_app/views.py
+++ main_app/views.py
@@ -92,18 +92,16 @@ def signup(request):
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
 
+@login_required
 def add_photo(request, client_id):
     photo_file = request.FILES.get('photo-file', None)
     if photo_file:
         s3 = boto3.client('s3')
-        # need a unique "key" for S3 / needs image file extension too
         key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
-        # just in case something goes wrong
         try:
             s3.upload_fileobj(photo_file, BUCKET, key)
-            # build the full url string
             url = f"{S3_BASE_URL}{BUCKET}/{key}"
-            # we can assign to cat_id or cat (if you have a cat object)
+            
             photo = Photo(url=url, client_id=client_id)
             photo.save()
         except:
